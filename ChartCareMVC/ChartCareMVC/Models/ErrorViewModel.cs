f12bccfb51e06ee4c2b887ab0735ab86f81abb7a
seanbsu <seancalkins@u.boisestate.edu>
2024-08-14 14:21:47 -0600

    MVC project Starter code

---
+++ ChartCareMVC/ChartCareMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChartCareMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
