3cdd2b9903880bd20d443236bf27f6f96d533be5
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2024-08-21 01:24:35 -0400

    Issue9: Added Plan and Companies model.

---
+++ ChartCareMVC/ChartCareMVC/Models/CompaniesModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChartCareMVC.Models
+{
+    public class CompaniesModel
+    {
+        [Required]
+        public required string CompanyId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Address { get; set; }
+
+        [ForeignKey("Plan")]
+        public string? PlanId { get; set; }
+
+        public virtual PlanModel? Plan { get; set; }
+
+        public string? AdminID {  get; set; }
+    }
+}
---
+++ ChartCareMVC/ChartCareMVC/Models/PlanModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChartCareMVC.Models
+{
+    public class PlanModel
+    {
+        [Required]
+        public required string PlanID { get; set; }
+
+        public string? Description { get; set; }
+
+        public float? MonthlyCost { get; set; }
+
+        public virtual ICollection<CompaniesModel>? Companies { get; set; }
+    }
+}
