3d0b0ffe2ed0a0ed4fe8bbbd2e7782daea0d6ae2
seanbsu <seancalkins@u.boisestate.edu>
2024-08-28 11:08:17 -0600

    set up smtp to send registration confirmation email

--- ChartCareMVC/ChartCareMVC/Program.cs
+++ ChartCareMVC/ChartCareMVC/Program.cs
@@ -3,6 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ChartCareMVC.Data;
 using ChartCareMVC.Areas.Identity.Data;
+using ChartCareMVC.Configurations;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using ChartCareMVC.Services;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("CompanyDbContextConnection") ?? throw new InvalidOperationException("Connection string 'CompanyDbContextConnection' not found.");
@@ -17,6 +22,12 @@ builder.Services.AddDefaultIdentity<CompanyUser>(options => options.SignIn.Requi
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+// Register SmtpSettings as a singleton service
+builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
+
+// Register EmailSender with the DI container
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
