4500583996fee5158fc738660fcb40d43fc855b8
seanbsu <seancalkins@u.boisestate.edu>
2024-09-09 11:00:05 -0600

    added a Features Service for anything that needs just the features table for

---
+++ ChartCareMVC/ChartCareMVC/Services/FeaturesService/FeatureService.cs
@@ -0,0 +1,42 @@
+﻿using ChartCareMVC.Data;
+using ChartCareMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ChartCareMVC.Services.FeaturesService
+{
+    public class FeatureService : IFeatureService
+    {
+        private readonly CompanyDbContext _dbContext;
+
+        public FeatureService(CompanyDbContext context) {
+            _dbContext = context;
+        }
+
+        public async Task<Result<List<Features>>> GetAllUniqueFeatures()
+        {
+            List<Features> features = await _dbContext.Features.ToListAsync();
+            if (features.IsNullOrEmpty()) {
+                return new Result<List<Features>>
+                {
+                    Success = false,
+                    ErrorMessage = "Features returned empty"
+                };
+            }
+            features.RemoveAll( f => f.Name.ToLower().Contains("employee count"));
+            features.Add(new Features { 
+                ID = 1,
+                Name = "Workforce Account Management",
+                Description = "Easily manage your company’s employee accounts with comprehensive tools for creation, deletion, and role assignment. " +
+                "This feature allows administrators to efficiently handle the entire lifecycle of workforce accounts, ensuring that each employee has" +
+                " the appropriate roles and access levels needed to perform their duties effectively."
+            });
+
+            return new Result<List<Features>>
+            {
+                Success =true,
+                Data = features
+            };
+        }
+    }
+}
---
+++ ChartCareMVC/ChartCareMVC/Services/FeaturesService/IFeatureService.cs
@@ -0,0 +1,18 @@
+﻿using ChartCareMVC.Models;
+namespace ChartCareMVC.Services.FeaturesService
+{
+    public interface IFeatureService
+    {
+        /// <summary>
+        /// Asynchronously retrieves a list of all unique features from the database. 
+        /// The method counts each employee account's features as a single unique feature.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task{Result{List{Features}}}"/> representing the asynchronous operation. 
+        /// The task result contains a <see cref="Result{List{Features}}"/> object, which encapsulates 
+        /// the result of the operation. The result will include a list of <see cref="Features"/> objects 
+        /// that represent the unique features found in the database.
+        /// </returns>
+        Task<Result<List<Features>>> GetAllUniqueFeatures();
+    }
+}
