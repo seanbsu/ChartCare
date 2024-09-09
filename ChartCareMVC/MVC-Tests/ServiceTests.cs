071d4c4b640b7e2c410ac61d09c887c9e84e2caf
seanbsu <seancalkins@u.boisestate.edu>
2024-09-09 11:40:13 -0600

    created class to test added services and their methods

---
+++ ChartCareMVC/MVC-Tests/ServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using ChartCareMVC.Services.FeaturesService;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Tests
+{
+    public class ServiceTests : TestHelpers
+    {
+
+        [Fact]
+        public async Task TestGetFeaturesNotNullAync()
+        {
+            // Arrange
+            var serviceProvider = ConfigureServices();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var serviceProviderInScope = scope.ServiceProvider;
+                SeedDatabase(serviceProviderInScope);
+
+                var featureService = serviceProviderInScope.GetRequiredService<IFeatureService>();
+
+                // Act
+                var result = await featureService.GetAllUniqueFeatures();
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.True(result.Success);
+                Assert.NotNull(result.Data);
+            }
+        }
+
+
+    }
+}
